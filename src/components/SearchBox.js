5c808f969d36a1381722c017dd530837da702621
egenejosh <92093251+egenejosh@users.noreply.github.com>
2022-07-13 20:02:30 +0100

    Robofriends commit

---
+++ src/components/SearchBox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SearchBox = ({searcfield,searchChange}) => {
+	return(
+		<div className='pa2'>
+			<input
+				className='pa3 ba b--green bg-lightest-blue'
+				type='search' 
+				placeholder='search robots'
+				onChange={searchChange}
+			/>
+		</div>
+	);
+}
+export default SearchBox; 
