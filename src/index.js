2b1adc00bda15ac9f992f8bafb685a1ddc393cf3
egenejosh <92093251+egenejosh@users.noreply.github.com>
2022-07-14 12:26:36 +0100

    fixing security issues and updating to react 18

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App tab="home" />, document.getElementById('root'));
 
 reportWebVitals();
