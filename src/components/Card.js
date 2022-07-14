f0a705075854ca8cb611f32de8c8bdd220454922
egenejosh <92093251+egenejosh@users.noreply.github.com>
2022-07-14 08:03:54 +0100

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Card = ({name, email, id}) => {
 	return(
-		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+		<div className='tc bg-light-green dib br4 pa3 ma2 grow bw2 shadow-5'>
 		 	<img alt='robots ' src={`https://robohash.org/${id}?200x200`} />
 			<div>
-				<h2>{name}</h2>
-				<p>{email}</p>
+				<h2>{Egene Joshua}</h2>
+				<p>{egenejosh@gmail.com}</p>
 			</div>
 		</div>
 	);
